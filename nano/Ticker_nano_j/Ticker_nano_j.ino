6c6d47fc0ad6326c77f8ac6cd29e2aae07072cd8
Jose Carlos Santamaria Poza <jcspoza@gmail.com>
2017-09-30 14:36:22 +0200

    Add files via upload

---
+++ nano/Ticker_nano_j/Ticker_nano_j.ino
@@ -0,0 +1,71 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+
+#define DEBUG
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)     Serial.print (x)
+ #define DEBUG_PRINTDEC(x)     Serial.print (x, DEC)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+ #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTDEC(x)
+ #define DEBUG_PRINTLN(x) 
+#endif
+
+int pinCS = 7; // Attach CS to this pin, DIN to MOSI and CLK to SCK por el conector ICSPI
+int numberOfHorizontalDisplays = 1;
+int numberOfVerticalDisplays = 1;
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+String tape = "Jose";
+int wait = 500; // In milliseconds
+
+int spacer = 1;
+int width = 5 + spacer; // The font width is 5 pixels
+
+void setup() {
+
+  Serial.begin (9600);
+  Serial.println("Skecht :Tiker_nano_j  Rotacion 3");
+  
+  matrix.setIntensity(7); // Use a value between 0 and 15 for brightness
+
+// Adjust to your own needs
+matrix.setPosition(0, 0, 0); // The first display is at <0, 0>
+//  matrix.setPosition(1, 1, 0); // The second display is at <1, 0>
+//  matrix.setPosition(2, 2, 0); // The third display is at <2, 0>
+//  matrix.setPosition(3, 3, 0); // And the last display is at <3, 0>
+//  ...
+matrix.setRotation(0, 3);    // The first display is position 90 counter clock
+//  matrix.setRotation(3, 2);    // The same hold for the last display
+}
+
+void loop() {
+
+  for ( int i = 0 ; i < width * tape.length() + matrix.width() - 1 - spacer; i++ ) {
+    DEBUG_PRINT("contador i ");
+    DEBUG_PRINTDEC(i);
+    DEBUG_PRINTLN("");
+    matrix.fillScreen(LOW);
+
+    int letter = i / width;
+    int x = (matrix.width() - 1) - i % width;
+    int y = (matrix.height() - 8) / 2; // center the text vertically
+
+    while ( x + width - spacer >= 0 && letter >= 0 ) {
+      if ( letter < tape.length() ) {
+        matrix.drawChar(x, y, tape[letter], HIGH, LOW, 1);
+      }
+
+      letter--;
+      x -= width;
+    }
+
+    matrix.write(); // Send bitmap to display
+
+    delay(wait);
+  }
+}
+
